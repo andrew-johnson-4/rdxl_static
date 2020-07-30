8933c4f9575a9c6fe00399e0a28a478f377794f7
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-29 17:17:45 -0700

    bump with passthrough changes to macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
 pub use rdxl_static_macros::*;
-
-pub fn dot_to_file<P: Into<String>, D: Into<String>>(p: P, dat: D) -> std::io::Result<()> {
-   let p: String = p.into();
-   let dat: String = dat.into();
-   let pr = p.rfind("/").expect("dot_to_file expected a / in directory path");
-   let pd = p[..pr].to_string();
-   let p = std::path::Path::new(&p);
-   std::fs::create_dir_all(pd)?;
-   let mut f = std::fs::File::create(p)?;
-   std::io::Write::write_all(&mut f, dat.as_bytes())?;
-   Ok(())
-}
--- tests/www.rs
+++
@@ -1,8 +0,0 @@
-
-#[test]
-fn dot1() {
-   if std::path::Path::new("www").exists() {
-      std::fs::remove_dir_all("www").unwrap();
-   }
-   rdxl_static::dot_to_file("www/index.html", "<p>Index file</p>").unwrap();
-}
